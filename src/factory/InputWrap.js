ddc8b20aeabde875a7e9d229f992297ce6f716f3
dishuostec <dishuostec@gmail.com>
2019-05-31 11:59:50 +0800

    添加 inputmask

--- src/factory/InputWrap.js
+++ src/factory/InputWrap.js
@@ -1,5 +1,6 @@
 import m from 'mithril';
 import classNames from 'classnames';
+import Inputmask from 'inputmask';
 
 const defaultFormat = (value) => {
   return value;
@@ -12,9 +13,21 @@ export const InputWrapFactory = (inputType, { defaultFilter, defaultConverter } 
     let isFocus = false;
 
     return {
+      oncreate: (vnode) => {
+        if (vnode.attrs.mask) {
+          Inputmask(vnode.attrs.mask).mask(vnode.dom);
+        }
+      },
+
+      onremove: (vnode) => {
+        if (vnode.dom.inputmask) {
+          vnode.dom.inputmask.remove();
+        }
+      },
+
       view: (vnode) => {
         const { attrs } = vnode;
-        const { class: className, value, filter, converter, format, oninput, onfocus, onblur, ...props } = attrs;
+        const { class: className, value, filter, converter, format, oninput, onfocus, onblur, mask, ...props } = attrs;
 
         const formater = format || defaultFormat;
 
